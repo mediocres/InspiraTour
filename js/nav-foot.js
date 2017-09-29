0f95b2af837094d5ca8aa176a1b2249aa9ad6680
Brian Geyer <geyerbri@msu.edu>
2017-09-29 16:42:37 -0400

    attempt 10

--- js/nav-foot.js
+++ js/nav-foot.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
 	$("#central-foot").load("footer.html");
 	var url = window.location.pathname;
 	var filename = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
-	var active = document.getElementsByClassName("filename");
+	var active = document.getElementsByClassName(filename);
 	active[0].classList.add("active");
 });
