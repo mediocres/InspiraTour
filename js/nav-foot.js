86b0b4653cddc98d458f7d10c1df6bde7db9a892
Brian Geyer <geyerbri@msu.edu>
2017-09-29 17:04:14 -0400

    fix typo attempt 11

--- js/nav-foot.js
+++ js/nav-foot.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
 		var filename = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
 		var active = document.getElementsByClassName(filename);
 		active[0].classList.add("active");
-	);
+	});
 	$("#central-foot").load("footer.html");
 });
