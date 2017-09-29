47a5c7c5fa2112f04b25b536fe67254b607c45be
Brian Geyer <geyerbri@msu.edu>
2017-09-29 17:28:51 -0400

    try changing class to id in navbar

--- js/nav-foot.js
+++ js/nav-foot.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 		var url = window.location.pathname;
 		var filename = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
 		var active = document.getElementById(filename);
-		active[0].classList.add("active");
+		active.classList.add("active");
 	});
 	$("#central-foot").load("footer.html");
 });
