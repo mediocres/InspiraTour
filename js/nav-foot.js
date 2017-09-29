8ec76a238adb5b7e35b46bc7623ee09df0128a87
Brian Geyer <geyerbri@msu.edu>
2017-09-29 17:16:56 -0400

    revert aboutus to about; change navbar classes to ids
    adjust css accordingly

--- js/nav-foot.js
+++ js/nav-foot.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#central-nav").load("navbar.html", function(){
 		var url = window.location.pathname;
 		var filename = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
-		var active = document.getElementsByClassName(filename);
+		var active = document.getElementById(filename);
 		active[0].classList.add("active");
 	});
 	$("#central-foot").load("footer.html");
