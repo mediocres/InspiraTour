a6b26cab1e3c1bd75290468a36c91e8642535bc5
Brian Geyer <geyerbri@msu.edu>
2017-10-06 10:45:37 -0400

    typo in last try

--- js/nav-foot.js
+++ js/nav-foot.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 		var url = window.location.pathname;
 		var filename = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
 		var active = document.getElementById(filename);
-		if (active.ClassList != null){
+		if (active.classList != null){
 			active.classList.add("active");
 		};
 	});
